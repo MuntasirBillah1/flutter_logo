6cf4a1c72f3662466acb8ff25983bf45b1762a7e
Jumail <jumail@utm.my>
2020-02-19 12:53:08 +0800

    Project starter

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+//! Todo: Add flutter logo
+//! Todo: Make circular logo
+//! Todo: Change size and color
+
+void main(){
+  print ('This print command will output to the PC, not to the emulator'); // To be removed later
+}
+
