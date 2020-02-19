b93db44b0a2360ecaec12ffb71655c0d5a276d66
Jumail <jumail@utm.my>
2020-02-19 12:55:32 +0800

    Add flutter logo

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
 
 //! Todo: Add flutter logo
-//! Todo: Make circular logo
-//! Todo: Change size and color
 
 void main(){
-  print ('This print command will output to the PC, not to the emulator'); // To be removed later
+  return runApp(FlutterLogo());
 }
 
